cbbbef4f2a9fa79e5dab873fdb3a0bebeaf81850
YiTingLee <ranye0719@gmail.com>
2017-05-31 16:23:28 +0800

    add effects

--- src/index.js
+++ src/index.js
@@ -15,30 +15,42 @@ const app = dva();
 app.model({
   namespace: 'count',
   state: {
-    record : 0,
+    record: 0,
     current: 0,
   },
-   reducers: {
-   add(state) {
-     const newCurrent = state.current + 1;
-     return { ...state,
-       record: newCurrent > state.record ? newCurrent : state.record,
-       current: newCurrent,
-     };
-   },
-   minus(state) {
-     return { ...state, current: state.current - 1};
-   },
- },
+  reducers: {
+    add(state) {
+      const newCurrent = state.current + 1;
+      return {
+        ...state,
+        record: newCurrent > state.record ? newCurrent : state.record,
+        current: newCurrent,
+      };
+    },
+    minus(state) {
+      return { ...state, current: state.current - 1 };
+    },
+  },
+  effects: {
+    *add(action, { call, put }) {
+      yield call(delay, 1000);
+      yield put({ type: 'minus' });
+    },
+  },
+  // subscriptions: {
+  //   keyboardWatcher({ dispatch }) {
+  //     key('⌘+up, ctrl+up', () => { dispatch({type:'add'}) });
+  //   },
+  // },
 });
 
-const CountApp = ({count, dispatch}) => {
+const CountApp = ({ count, dispatch }) => {
   return (
     <div className={styles.normal}>
       <div className={styles.record}>Highest Record: {count.record}</div>
       <div className={styles.current}>{count.current}</div>
       <div className={styles.button}>
-        <button onClick={() => { dispatch({type: 'count/add'}); }}>+</button>
+        <button onClick={() => { dispatch({ type: 'count/add' }); }}>+</button>
       </div>
     </div>
   );
@@ -59,3 +71,8 @@ app.router(({ history }) =>
 // 4. Start
 app.start('#root');
 
+function delay(timeout) {
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout);
+  });
+}
