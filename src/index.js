ad793aa9aba3539f52f4e712306cbb6262d07b38
YiTingLee <ranye0719@gmail.com>
2017-05-31 15:42:33 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+import dva, { connect } from 'dva';
+import { Router, Route } from 'dva/router';
+import fetch from 'dva/fetch';
+import React from 'react';
+import './index.html';
+import styles from './index.less';
+
+// 1. Initialize
+const app = dva();
+
+// 2. Model
+// Remove the comment and define your model.
+//app.model({});
+
+app.model({
+  namespace: 'count',
+  state: {
+    record : 0,
+    current: 0,
+  },
+   reducers: {
+   add(state) {
+     const newCurrent = state.current + 1;
+     return { ...state,
+       record: newCurrent > state.record ? newCurrent : state.record,
+       current: newCurrent,
+     };
+   },
+   minus(state) {
+     return { ...state, current: state.current - 1};
+   },
+ },
+});
+
+const CountApp = ({count, dispatch}) => {
+  return (
+    <div className={styles.normal}>
+      <div className={styles.record}>Highest Record: {count.record}</div>
+      <div className={styles.current}>{count.current}</div>
+      <div className={styles.button}>
+        <button onClick={() => { dispatch({type: 'count/add'}); }}>+</button>
+      </div>
+    </div>
+  );
+};
+
+function mapStateToProps(state) {
+  return { count: state.count };
+}
+
+// 3. Router
+const HomePage = connect(mapStateToProps)(CountApp);
+app.router(({ history }) =>
+  <Router history={history}>
+    <Route path="/" component={HomePage} />
+  </Router>
+);
+
+// 4. Start
+app.start('#root');
+
